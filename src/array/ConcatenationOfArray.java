7a3cc426f0c36ae98297fe360ddb0af50029582b
Karthik <karthik.p6603@gmail.com>
2026-08-13 16:03:40 +0530

    concatenation

---
+++ src/array/ConcatenationOfArray.java
@@ -0,0 +1,11 @@
+package array;
+
+public class ConcatenationOfArray {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -1,5 +1,10 @@
 package array;
 
-public class TwoSum {
+import java.util.Scanner;
 
+public class TwoSum {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+    }
 }
