45bce6b6645c637c7ab983a4faba530c4949e153
Karthik <karthik.p6603@gmail.com>
2026-08-16 11:39:55 +0530

    order agnostice binary search

---
+++ src/BinarySearch/OrderAgnosticBinarySearch.java
@@ -0,0 +1,9 @@
+package BinarySearch;
+
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+        // this is the order agnostic binary search in whch the order of the array is either sorted in the ascending order or the descending order
+        // we can find this by comparing the first and last element in the array
+
+    }
+}
