2cd5bdff021cab2e8ca2624753c9cd49673fc68b
Karthik <karthik.p6603@gmail.com>
2026-08-20 21:19:47 +0530

    bs add

--- src/LInearSearch/LinearSearch.java
+++ src/LInearSearch/LinearSearch.java
@@ -3,5 +3,6 @@ package LInearSearch;
 public class LinearSearch {
     public static void main(String[] args) {
         System.out.println("This is where the linear search is done");
+        // and this is the problemn
     }
 }
