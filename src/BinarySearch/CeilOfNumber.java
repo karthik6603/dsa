4827d341934abb0239c2193add96226515c98c3a
Karthik <karthik.p6603@gmail.com>
2026-08-22 11:11:52 +0530

    add floorof number

--- src/BinarySearch/CeilOfNumber.java
+++ src/BinarySearch/CeilOfNumber.java
@@ -2,4 +2,5 @@ package BinarySearch;
 
 public class CeilOfNumber {
     // This is the smallest number which is greater than or equal to the target element
+
 }
---
+++ src/BinarySearch/FloorOfNumber.java
@@ -0,0 +1,7 @@
+package BinarySearch;
+
+public class FloorOfNumber {
+    public static void main(String[] args) {
+        // this is the largest element which are less than are equal to the target
+    }
+}
