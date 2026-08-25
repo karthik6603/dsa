ce24b0387a3f726c26d1bf83bb77feeb8df53e90
Karthik <karthik.p6603@gmail.com>
2026-08-25 21:37:23 +0530

    add linear search

--- src/BinarySearch/FloorOfNumber.java
+++ src/BinarySearch/FloorOfNumber.java
@@ -3,5 +3,6 @@ package BinarySearch;
 public class FloorOfNumber {
     public static void main(String[] args) {
         // this is the largest element which are less than are equal to the target
+
     }
 }
---
+++ src/LInearSearch/FindElement.java
@@ -0,0 +1,7 @@
+package LInearSearch;
+
+public class FindElement {
+    public static void main(String[] args) {
+        // this is where the input of the part of the code goes in
+    }
+}
