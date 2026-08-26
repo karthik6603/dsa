95088bb3453cf12f5b774e954beaab63ac56ff26
Karthik <karthik.p6603@gmail.com>
2026-08-26 22:38:08 +0530

    Update FindElement.java

--- src/LInearSearch/FindElement.java
+++ src/LInearSearch/FindElement.java
@@ -2,6 +2,6 @@ package LInearSearch;
 
 public class FindElement {
     public static void main(String[] args) {
-        // this is where the input of the part of the code goes in
+        // this is where the input of the part of the code goes in this is one of the basic program that can you do
     }
 }
