5fb9a1b8382cae8e71336db161596fb2a5c0dc6b
Karthik <karthik.p6603@gmail.com>
2026-08-28 20:19:59 +0530

    add reverse array

--- src/array/RotateArray.java
+++ src/array/RotateArray.java
@@ -3,5 +3,6 @@ package array;
 public class RotateArray {
     public static void main(String[] args) {
         // rotate the array by k steps
+        // if the array is roated to the left we have to use the reverse the array ane then the partial reversal of the parts till the k
     }
 }
