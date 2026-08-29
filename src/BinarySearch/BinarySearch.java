2442f8ce2f4a34f165e500803cbe4618db8f133f
Karthik <karthik.p6603@gmail.com>
2026-08-29 23:22:38 +0530

    Update BinarySearch.java

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -2,5 +2,5 @@ package BinarySearch;
 
 public class BinarySearch {
 
-    // This will be the normal binary search;
+    // This will be the normal binary search; to prevent the overflow if integers we use
 }
