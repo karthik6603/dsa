c7d02ddefaa59915d7c074c71b1f60825465c753
Karthik <karthik.p6603@gmail.com>
2026-08-31 21:54:58 +0530

    floor of a number

--- src/BinarySearch/FloorOfNumber.java
+++ src/BinarySearch/FloorOfNumber.java
@@ -1,6 +1,11 @@
 package BinarySearch;
 
 public class FloorOfNumber {
+
+    private static int binarySearch(int[] arr, int target){
+        return -1;
+    }
+
     public static void main(String[] args) {
         // this is the largest element which are less than are equal to the target
 
