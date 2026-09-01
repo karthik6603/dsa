6eff6338cba82e2f1843267095af967b246f9ce2
Karthik <karthik.p6603@gmail.com>
2026-09-01 21:36:15 +0530

    binary search of first occurence

--- src/BinarySearch/FloorOfNumber.java
+++ src/BinarySearch/FloorOfNumber.java
@@ -3,6 +3,23 @@ package BinarySearch;
 public class FloorOfNumber {
 
     private static int binarySearch(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
         return -1;
     }
 
